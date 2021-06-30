52c5aa7ef00a64d12e70fa3055b23306f345ac46
Izabela Żmujdzin <izabela.zmujdzin@gmail.com>
2021-06-30 13:31:52 +0200

    changed colors

--- src/js/index.js
+++ src/js/index.js
@@ -10,11 +10,13 @@ const textarea = document.querySelector('.textarea--js');
 const save = document.querySelector('.save--js');
 const load = document.querySelector('.load--js');
 
+
 save.addEventListener('click', (e) => {
 	e.preventDefault();
 	localStorage.setItem('entry', textarea.value);
 });
 
-load.addEventListener('click', () => {
+load.addEventListener('click', (e) => {
+	e.preventDefault();
 	textarea.value = localStorage.getItem('entry');
 });
